de7545e4a5582cbf64705dc80c688f9e67056ee6
Reyshaun Medina <170153734+reyshaun-medina@users.noreply.github.com>
2026-03-31 01:07:17 -0400

    Update QueueFullException.java

--- QueueFullException.java
+++ QueueFullException.java
@@ -1,13 +1,18 @@
-package lab5;
+package lab5b;
 
+/**
+ * Exception thrown when an enqueue is attempted on a full queue.
+ */
 public class QueueFullException extends RuntimeException {
 	private String message;
 	
 	public QueueFullException() {
+		// default message used when no custom text is provided
 		message = "Queue is full.";
 	}
 	
 	public QueueFullException(String s) {
+		// allow custom message text
 		message = s;
 	}
 	
