ec86c8ed01d76b2b286d2763c8bf972d9493c1aa
Reyshaun Medina <170153734+reyshaun-medina@users.noreply.github.com>
2026-03-31 01:06:30 -0400

    Refactor Queue interface and add documentation
    Updated the Queue interface with detailed documentation and moved it to a new package.

--- Queue.java
+++ Queue.java
@@ -1,10 +1,42 @@
-package lab5;
+package lab5b;
 
+/**
+ * Simple queue interface used by the simulation. Implementations may be
+ * thread-safe (like ArrayQueue) and throw RuntimeException or specific
+ * QueueEmptyException/QueueFullException on error conditions.
+ *
+ * @param <T> the type stored in the queue
+ */
 public interface Queue<T> {
+	/**
+	 * Insert an item at the rear of the queue.
+	 * Implementations should document concurrency semantics.
+	 * @param item the item to add
+	 * @throws RuntimeException or QueueFullException if the queue cannot accept the item
+	 */
 	void enqueue(T item) throws RuntimeException;
+	/**
+	 * Remove and return the item at the front of the queue.
+	 * @return the removed item
+	 * @throws RuntimeException or QueueEmptyException if the queue is empty
+	 */
 	T dequeue() throws RuntimeException;
+	/**
+	 * Peek at the front element without removing it.
+	 * @return the front element
+	 * @throws RuntimeException if the queue is empty
+	 */
 	T front() throws RuntimeException;
+	/**
+	 * @return current number of items in the queue
+	 */
 	int getSize();
+	/**
+	 * @return true if the queue buffer is at capacity
+	 */
 	boolean isFull();
+	/**
+	 * @return true if there are no items in the queue
+	 */
 	boolean isEmpty();
 }
